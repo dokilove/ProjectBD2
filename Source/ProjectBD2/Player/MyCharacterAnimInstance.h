b9d5bac1e93438bb4e2025576e7db426814d1366
hjshim <dokilove2@gmail.com>
2018-05-17 18:16:07 +0900

    AnimInstance Cpp 추가

---
+++ Source/ProjectBD2/Player/MyCharacterAnimInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "MyCharacterAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBD2_API UMyCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
