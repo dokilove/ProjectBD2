a6786514aad73219159cb18b54d9b2c75dddcd7f
dokilove <dokilove2@gmail.com>
2018-05-18 21:58:20 +0900

    Crosshair 추가

---
+++ Source/ProjectBD2/Basic/MyHUD.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyHUD.h"
+#include "Engine/Texture2D.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Engine/Canvas.h"
+
+
+AMyHUD::AMyHUD()
+{
+	static ConstructorHelpers::FObjectFinder<UTexture2D> T_Crosshair(TEXT("Texture2D'/Game/TPSData/Weapons/crosshair.crosshair'"));
+	if (T_Crosshair.Succeeded())
+	{
+		Crosshair = T_Crosshair.Object;
+	}
+}
+
+void AMyHUD::DrawHUD()
+{
+	Super::DrawHUD();
+	if (Crosshair)
+	{
+		DrawTextureSimple(Crosshair,
+			Canvas->SizeX/2 - Crosshair->GetSizeX() / 2,
+			Canvas->SizeY/2 - Crosshair->GetSizeY() / 2);
+
+	}
+}
---
+++ Source/ProjectBD2/Basic/MyHUD.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "MyHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBD2_API AMyHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+public:
+	AMyHUD();
+	virtual void DrawHUD() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class UTexture2D* Crosshair;
+};
